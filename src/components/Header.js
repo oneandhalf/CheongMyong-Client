0538292e279a91c8f55f37f9b5f3a3dd92676401
Jongwoo-Han <han980817@gmail.com>
2019-10-31 10:35:44 +0900

    Added Component: Header

---
+++ src/components/Header.js
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+import {
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import "./Header.css"
+
+const Header = props => {
+  const [isOpen,
+    setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const [menuItem] = useState([
+    {
+      id: 1,
+      title: "홈",
+      href: "/"
+    }, {
+      id: 2,
+      title: "청명이란",
+      href: "/"
+    }, {
+      id: 3,
+      title: "청원하기",
+      href: "/"
+    }, {
+      id: 4,
+      title: "나의 청원",
+      href: "/"
+    }, {
+      id: 5,
+      title: "자주하는 질문",
+      href: "/"
+    }
+  ]);
+
+  let _menuItem = [];
+  for (let i = 0; i < menuItem.length; i++) {
+    _menuItem.push(
+      <NavItem key={menuItem[i].id}>
+        <NavLink href={menuItem[i].href}>{menuItem[i].title}</NavLink>
+      </NavItem>
+    );
+  }
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand href="/">Logo</NavbarBrand>
+          <NavbarToggler onClick={toggle}/>
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              {_menuItem}
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
