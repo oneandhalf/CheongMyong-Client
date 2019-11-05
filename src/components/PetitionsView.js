445980a577950658309cd22a15eb4550842f4b7b
Jongwoo-Han <han980817@gmail.com>
2019-11-05 23:29:22 +0900

    Modified Component: PetitionsView

--- src/components/PetitionsView.js
+++ src/components/PetitionsView.js
@@ -1,39 +1,50 @@
-import React, { useState } from "react";
-import { Nav, NavItem, NavLink } from "reactstrap";
+import React, {useState} from "react";
+import {Nav, NavItem, NavLink} from "reactstrap";
+import classNames from "classnames";
 import "./assets/css/PetitionsView.css";
 
 const PetitionsView = props => {
-  const [petitionsMenu] = useState([
+  const [pMenu] = useState([
     {
       id: 1,
       title: "분야별 청원",
       desc: "Field"
-    },
-    {
+    }, {
       id: 2,
       title: "추천순 청원",
       desc: "Recommend"
-    },
-    {
+    }, {
       id: 3,
       title: "답변된 청원",
       desc: "Reply"
     }
   ]);
 
-  let _petitionsMenu = [];
+  const [pMenuOn, setPMenuOn] = useState([true, false, false]);
+  let bList = [false, false, false];
 
-  for (let i = 0; i < petitionsMenu.length; i++) {
-    _petitionsMenu.push(
-      <NavItem key={petitionsMenu[i].id} className="petitions-menubar">
-        <NavLink>{petitionsMenu[i].title}</NavLink>
+  let _pMenu = [];
+  for (let i = 0; i < pMenu.length; i++) {
+    _pMenu.push(
+      <NavItem
+        key={pMenu[i].id}
+        className={classNames("petitions-menubar", {on: pMenuOn[i]})}>
+        <NavLink
+          onClick={e => {
+          e.preventDefault();
+          
+          bList.splice(i, 1, true);
+          setPMenuOn(bList);
+        }}>
+          {pMenu[i].title}
+        </NavLink>
       </NavItem>
     );
   }
   return (
     <div className="PetitionsView">
       <Nav>
-        {_petitionsMenu}
+        {_pMenu}
       </Nav>
     </div>
   );
