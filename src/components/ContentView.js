214654c55e9d3431081492076ae4176ed95c1f9c
Jongwoo-Han <han980817@gmail.com>
2019-11-05 21:46:59 +0900

    Modified Component: ContentView

--- src/components/ContentView.js
+++ src/components/ContentView.js
@@ -1,16 +1,32 @@
-import React, {useState} from "react";
+import React from "react";
 import {Container} from "reactstrap";
 import "./assets/css/ContentView.css"
 import MainImg from "./MainImg";
+import PetitionsView from "./PetitionsView";
 
 const ContentView = props => {
-  const [content,
-    setContent] = useState();
   let contentMode = props.mode;
+  let _content = [];
+
+  switch (contentMode) {
+    case "MyPage":
+
+      break;
+    case "FAQ":
+
+      break;
+    case "introduce":
+      _content.push(<MainImg/>);
+      break;
+    default:
+      _content.push(
+        <MainImg/>, < PetitionsView/>);
+      break;
+  }
 
   return (
     <Container fluid={true} className="cont-fluid">
-      <MainImg/>
+      {_content}
     </Container>
   );
 }
