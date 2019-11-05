fe924d97229e1624330bfb4c9c9b26d90e04f7b5
Jongwoo-Han <han980817@gmail.com>
2019-11-05 21:46:16 +0900

    Modified Component: MainImg

--- src/components/MainImg.js
+++ src/components/MainImg.js
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
 import "./assets/css/MainImg.css";
 
 const MainImg = props => {
   return (
-    <header class="header-bg">
-      
+    <header className="header-bg header-width">
+      <div className="header-cont cont-txt">
+        <h2 className="header-title title-size in-down">명지 청원</h2>
+      </div>
     </header>
   );
 };
