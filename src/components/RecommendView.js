4a6d42a955808dddea5ec7161fd14f4ca34ccedc
Jongwoo-Han <han980817@gmail.com>
2019-11-06 00:43:58 +0900

    Added Component: RecommendView

---
+++ src/components/RecommendView.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const RecommendView = props => {
+  return (
+    <div className="RecommendView">
+      
+    </div>
+  );
+};
+
+export default RecommendView;
