8165f3690ac96e2d30a7030eb6ad90d0416db48f
Jongwoo-Han <han980817@gmail.com>
2019-11-06 11:22:39 +0900

    Modified Component: App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React, {useState} from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
-import ContentView from "./components/ContentView";
+import Introduce from "./components/Introduce";
+import PetitionsView from "./components/PetitionsView";
+import MyPage from "./components/MyPage";
+import FAQ from "./components/FAQ";
+
 
 const App = props => {
-  const [mode,
-    setMode] = useState("petitions");
-  const [menuItem] = useState([
-    {
-      id: 1,
-      title: "청명이란",
-      desc: "introduce"
-    }, {
-      id: 2,
-      title: "청원하기",
-      desc: "petitions"
-    }, {
-      id: 3,
-      title: "나의 청원",
-      desc: "MyPage"
-    }, {
-      id: 4,
-      title: "자주하는 질문",
-      desc: "FAQ"
-    }
-  ]);
-
   return (
-    <div className="App">
-      <Header onChangeMode={mode => setMode(mode)} menu={menuItem}/>
-      <ContentView mode={mode}/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <div className="view-comp">
+            <Route exact path="/" component={PetitionsView} />
+            <Route path="/introduce" component={Introduce} />
+            <Route path="/MyPage" component={MyPage} />
+            <Route path="/FAQ" component={FAQ} />
+          </div>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
