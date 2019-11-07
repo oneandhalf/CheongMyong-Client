0f6754f26e229bf671b25a30cf220dd5bc17d2b8
Jongwoo-Han <han980817@gmail.com>
2019-11-07 15:54:47 +0900

    Modified Component: App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, {Fragment} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Introduce from "./components/Introduce";
-import PetitionsView from "./components/PetitionsView";
 import MyPage from "./components/MyPage";
 import FAQ from "./components/FAQ";
+import FieldView from "./components/FieldView";
+import RecommendView from "./components/RecommendView";
+import ReplyView from "./components/ReplyView";
 
 const App = props => {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header/>
         <Switch>
-          <div className="view-comp">
-            <Route exact path="/" component={Introduce} />
-            <Route path="/petitions" component={PetitionsView} />
-            <Route path="/MyPage" component={MyPage} />
-            <Route path="/FAQ" component={FAQ} />
-          </div>
+          <Fragment>
+            <div className="view-comp">
+              <Route exact path="/" component={Introduce}/>
+              <Route path="/field" component={FieldView}/>
+              <Route path="/recommend" component={RecommendView}/>
+              <Route path="/reply" component={ReplyView}/>
+              <Route path="/user" component={MyPage}/>
+              <Route path="/questions" component={FAQ}/>
+            </div>
+          </Fragment>
         </Switch>
       </div>
     </Router>
